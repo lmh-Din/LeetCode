4248ad0d5bc9c0bb05ff20d1cb0f3f5e8e408be9
Wazi <2863547436@qq.com>
2019-01-24 14:15:03 +0800

    init

---
+++ src/main/java/com/code/LeetCode/LeetCodeApplication.java
@@ -0,0 +1,14 @@
+package com.code.LeetCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCodeApplication.class, args);
+	}
+
+}
+
